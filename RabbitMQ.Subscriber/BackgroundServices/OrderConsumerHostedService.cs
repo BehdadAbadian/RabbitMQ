b880dfecaf9cefc176600a7ef6880aafad03fd67
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2024-11-30 20:26:19 +0330

    Implemented Subscriber Service

---
+++ RabbitMQ.Subscriber/BackgroundServices/OrderConsumerHostedService.cs
@@ -0,0 +1,35 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Subscriber.BackgroundServices;
+
+public class OrderConsumerHostedService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var orderQueueName = "order-queue";
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: orderQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            return Task.CompletedTask;
+            //call another service
+        };
+
+            await channel.BasicConsumeAsync(queue: orderQueueName, autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Waiting for feedback. Press [enter] to exit.");
+            Console.ReadLine();
+
+        
+    }
+}
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -1,7 +1,9 @@
+using RabbitMQ.Subscriber.BackgroundServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHostedService<OrderConsumerHostedService>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApiDocument();
